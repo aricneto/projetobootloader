d068f56b3c9a42c705cc7da7e775f5f698d04983
AriNeto <aricneto@gmail.com>
2018-04-05 09:36:44 -0300

    Did some things

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# projetobootloader
-# projetobootloader
-# projetobootloader
+# Projeto Bootloader
+Tipo um Guitar Hero só que em Assembly.
+
+# Sobre
+- `boot1.asm` inicia em `0x7c00` e carrega o setor de memória de boot2
+- `boot2.asm` inicia em `0x500` e carrega o setor de memória do kernel mas antes disso apresenta algumas mensagens para alertar o usuario do que está acontecendo
+- `kernel.asm` inicia em `0x7e00` e é onde o programa principal está armazenado
+- Para compilar, veja o makefile
+- Todos tem 512 bytes de memória disponível
+
+# Style
+- Identação: `4 espaços`
+- Espaço depois de toda virgula e ponto-e-virgula
+- Numeros hexadecimais são representados com `0x`, a não ser em caso de interrupção, onde devem ser representados por `h`
+    - Ex: `org 0x7c00`
+    - Ex: `int 10h`
+- Comentários próximos devem estar alinhados
+    - Ex:
+```
+xor si, si ; clear «s1»
+xor bx, bx ; clear «bx»
+cx, 1      ; set initial decimal place
+```
+- Comentar o que cada registrador configura antes de uma interrupção
+    - Ex: 
+```	
+; [int 10h 0x03] - read cursor and store in «dh», «dl»
+xor bh, bh    ; [video page] «bh» <- 0 
+mov ah, 0x03
+int 10h
+```
--- boot2.asm
+++ boot2.asm
@@ -6,31 +6,31 @@ start:
     mov ds, ax
     mov es, ax
 
-    mov ah,0xb
-    mov bh,0
-    mov bl,4
+    mov ah, 0xb
+    mov bh, 0
+    mov bl, 4
     int 10h
     jmp load_menu
 load_menu:
 ;Setando a posição do disco onde kernel.asm foi armazenado(ES:BX = [0x7E00:0x0])
-	mov ax,0x7E0	;0x7E0<<1 + 0 = 0x7E00
-	mov es,ax
-	xor bx,bx		;Zerando o offset
+    mov ax, 0x7E0	;0x7E0<<1 + 0 = 0x7E00
+    mov es,ax
+    xor bx,bx		;Zerando o offset
 
-;Setando a posição da Ram onde o menu será lido
-	mov ah, 0x02	;comando de ler setor do disco
-	mov al,4		;quantidade de blocos ocupados pelo menu
-	mov dl,0		;drive floppy
+;Setando a posição da RAM onde o menu será lido
+    mov ah, 0x02	;comando de ler setor do disco
+    mov al, 4		;quantidade de blocos ocupados pelo menu
+    mov dl, 0		;drive floppy
 
 ;Usaremos as seguintes posições na memoria:
-	mov ch,0		;trilha 0
-	mov cl,3		;setor 3
-	mov dh,0		;cabeca 0
-	int 13h
-	jc load_menu	;em caso de erro, tenta de novo
+    mov ch, 0		;trilha 0
+    mov cl, 3		;setor 3
+    mov dh, 0		;cabeca 0
+    int 13h
+    jc load_menu	;em caso de erro, tenta de novo
 
 break:
-	jmp 0x7e00		;Pula para a posição carregada
+    jmp 0x7e00		;Pula para a posição carregada
 
 times 510-($-$$) db 0
 dw 0xaa55
